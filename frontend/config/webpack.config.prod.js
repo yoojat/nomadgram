bbd30bbdf962672c950f92a511d5742f3cbabe12
yoojat <yooja.ty@gmail.com>
2018-01-10 19:34:46 +0900

    Creating the Footer Component

--- frontend/config/webpack.config.prod.js
+++ frontend/config/webpack.config.prod.js
@@ -183,6 +183,7 @@ module.exports = {
                         localIdentName:
                           '[path][name]__[local]--[hash:base64:5]',
                         sourceMap: shouldUseSourceMap,
+                        camelCase: 'dashes',
                       },
                     },
                     {
