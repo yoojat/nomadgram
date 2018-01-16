fab2f4fe9077c51d0bb9284e0075fc8527f54256
yoojat <yooja.ty@gmail.com>
2018-01-16 17:23:08 +0900

    Login In Redux Action

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -28,7 +28,28 @@ function facebookLogin(access_token) {
       .then(response => response.json())
       .then(json => {
         if (json.token) {
-          localStorage.setItem('jwt', json.token);
+          dispatch(saveToken(json.token));
+        }
+      })
+      .catch(err => console.log(err));
+  };
+}
+
+function usernameLogin(username, password) {
+  return function(dispatch) {
+    fetch('rest-auth/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+      .then(response => response.json())
+      .then(json => {
+        if (json.token) {
           dispatch(saveToken(json.token));
         }
       })
@@ -55,6 +76,8 @@ function reducer(state = initialState, action) {
 
 function applySetToken(state, action) {
   const {token} = action;
+  localStorage.setItem('jwt', token);
+
   return {
     ...state,
     isLoggedIn: true,
@@ -65,6 +88,7 @@ function applySetToken(state, action) {
 
 const actionCreators = {
   facebookLogin,
+  usernameLogin,
 };
 
 export {actionCreators};
